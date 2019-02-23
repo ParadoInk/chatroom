e1852c4f47c57965e9785902c5212e9e9a92d413
Parado <timeandparadox@gmail.com>
2019-02-23 17:48:07 -0500

    Finished Content

--- chatroomnode.js
+++ chatroomnode.js
@@ -6,7 +6,10 @@ var path = require('path')
 
 let nicks = {
 
-}
+};
+
+let typings = [];
+let messages = [];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,26 +18,72 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    let nick = "anon"
+    let nick = "anon";
+    let istyping = false;
     nicks["" + socket.id] = nick;
     console.log('a user has connected: ' + socket.id);
     io.emit('chat message', "a user has connected");
+    updateonlinelist();
+    for(let i = 0; i < messages.length; i++){
+        socket.emit('chat message', messages[i]);
+    }
     socket.on('disconnect', function(){
         console.log('user disconnected');
-        io.emit('chat message', "a user has disconnected");
+        io.emit('chat message', nicks["" + socket.id] + " has disconnected");
+        typeupdate(false);
         delete nicks["" + socket.id];
+        updateonlinelist();
+
     });
 
+
     socket.on('chat message', function(msg){
+        console.log(nicks);
         console.log('message: ' + msg + "\nfrom: " + socket.id);
         io.emit('chat message', nicks["" + socket.id] + ": " + msg);
+        if(messages.length < 100){
+            messages.push(nicks["" + socket.id] + ": " + msg);
+        }
+        else {
+            messages.pop();
+            messages.push(nicks["" + socket.id] + ": " + msg);
+        }
     });
 
     socket.on('change nick', function(newnick){
-        io.emit('chat message', nick + " has changed their name to " + newnick)
+        io.emit('chat message', nick + " has changed their name to " + newnick);
         nick = newnick;
         nicks["" + socket.id] = newnick;
-    })
+        updateonlinelist();
+        if(messages.length < 100){
+            messages.push(nick + " has changed their name to " + newnick);
+        }
+        else {
+            messages.pop();
+            messages.push(nick + " has changed their name to " + newnick);
+        }
+    });
+
+    function updateonlinelist(){
+        io.emit('online list', nicks);
+    }
+
+    function typeupdate(typing){
+        if(typing){
+            typings.push(nicks["" + socket.id]);
+        }
+        else {
+            for(let i = 0; i < typings.length; i++){
+                if(typings[i] == nicks["" + socket.id]){
+                    typings.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        console.log(typings);
+        socket.broadcast.emit('typinglist', typings);
+    }
+    socket.on('typing', function(typing){typeupdate(typing);});
 
 });
 
--- public/chatroom.js
+++ public/chatroom.js
@@ -1,6 +1,6 @@
-
-
 $(function () {
+    let typing = false;
+    let nick = "anon";
     var socket = io();
     $('#messagebox').submit(function(e){
         e.preventDefault(); // prevents page reloading
@@ -9,28 +9,72 @@ $(function () {
         return false;
     });
     $('#nickchanger').submit(function(e){
+        nick = document.getElementById('n').value;
         e.preventDefault();
         socket.emit('change nick', $('#n').val());
         $('#n').val('');
         return false;
-    })
+    });
+    socket.on('online list', function(list){
+        document.getElementById('online').innerHTML = "";
+       for(var thing in list){
+           $('#online').append($('<li>').text(list[thing]));
+       }
+    });
     socket.on('chat message', function(msg){
-        console.log($('#messagediv').scrollTop());
-        console.log(document.getElementById('messagediv').scrollHeight - window.innerHeight * .9);
-        console.log(document.getElementById('messagediv').scrollHeight);
-        console.log(window.innerHeight * .9);
         let scroll = true;
-        if(document.getElementById('messagediv').scrollHeight < window.innerHeight * .9){
+        let diff = Math.abs($('#messagediv').scrollTop() - (document.getElementById('messagediv').scrollHeight - window.innerHeight * .85));
+        if(document.getElementById('messagediv').scrollHeight < window.innerHeight * .85){
             scroll = false;
         }
-        let diff = Math.abs($('#messagediv').scrollTop() - (document.getElementById('messagediv').scrollHeight - window.innerHeight * .9));
         $('#messages').append($('<li>').text(msg));
+        console.log(diff);
         if(diff < 5){
             $('#messagediv').scrollTop($('#messagediv')[0].scrollHeight);
         }
-        else if(!scroll && document.getElementById('messagediv').scrollHeight > window.innerHeight * .9){
+        else if(!scroll && document.getElementById('messagediv').scrollHeight > window.innerHeight * .85){
             $('#messagediv').scrollTop($('#messagediv')[0].scrollHeight);
         }
     });
+    socket.on('typinglist', function(list){
+        let ourlist = list;
+        if(typing){
+            for(let i = 0; i < list.length; i++){
+                if(list[i] == nick){
+                    ourlist.splice(i, 1);
+                }
+                break;
+            }
+        }
+        document.getElementById('typing').innerHTML = "";
+        for(let i = 0; i < ourlist.length - 1; i++){
+            document.getElementById('typing').innerHTML += ourlist[i] + ", "
+        }
+        if(ourlist.length > 1){
+            document.getElementById('typing').innerHTML += " and " + ourlist[ourlist.length - 1] + " are typing.";
+        }
+        else if(ourlist.length == 1){
+            document.getElementById('typing').innerHTML = ourlist[0] + " is typing.";
+        }
+
+    });
+
+    function sendtyping(){
+        socket.emit('typing', typing);
+    }
+
+    function update(){
+        if(document.getElementById('m').value != "" && typing == false){
+            typing = true;
+            sendtyping();
+        }
+        else if(document.getElementById('m').value == "" && typing == true){
+            typing = false;
+            sendtyping();
+        }
+    }
+
+    setInterval(function(){ update(); }, 50);
+
 });
 
