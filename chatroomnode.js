d0dff04b7d7dc75af35c37d00ae319a6d19a5b24
Parado <timeandparadox@gmail.com>
2019-02-23 15:44:37 -0500

    Fixed CSS, added scrolling, added nickname change.

--- chatroomnode.js
+++ chatroomnode.js
@@ -4,6 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path')
 
+let nicks = {
+
+}
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
@@ -11,15 +15,26 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user has connected');
+    let nick = "anon"
+    nicks["" + socket.id] = nick;
+    console.log('a user has connected: ' + socket.id);
+    io.emit('chat message', "a user has connected");
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        io.emit('chat message', "a user has disconnected");
+        delete nicks["" + socket.id];
     });
 
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        console.log('message: ' + msg + "\nfrom: " + socket.id);
+        io.emit('chat message', nicks["" + socket.id] + ": " + msg);
     });
+
+    socket.on('change nick', function(newnick){
+        io.emit('chat message', nick + " has changed their name to " + newnick)
+        nick = newnick;
+        nicks["" + socket.id] = newnick;
+    })
 
 });
 
--- public/chatroom.js
+++ public/chatroom.js
@@ -2,14 +2,35 @@
 
 $(function () {
     var socket = io();
-    $('form').submit(function(e){
+    $('#messagebox').submit(function(e){
         e.preventDefault(); // prevents page reloading
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
     });
+    $('#nickchanger').submit(function(e){
+        e.preventDefault();
+        socket.emit('change nick', $('#n').val());
+        $('#n').val('');
+        return false;
+    })
     socket.on('chat message', function(msg){
+        console.log($('#messagediv').scrollTop());
+        console.log(document.getElementById('messagediv').scrollHeight - window.innerHeight * .9);
+        console.log(document.getElementById('messagediv').scrollHeight);
+        console.log(window.innerHeight * .9);
+        let scroll = true;
+        if(document.getElementById('messagediv').scrollHeight < window.innerHeight * .9){
+            scroll = false;
+        }
+        let diff = Math.abs($('#messagediv').scrollTop() - (document.getElementById('messagediv').scrollHeight - window.innerHeight * .9));
         $('#messages').append($('<li>').text(msg));
+        if(diff < 5){
+            $('#messagediv').scrollTop($('#messagediv')[0].scrollHeight);
+        }
+        else if(!scroll && document.getElementById('messagediv').scrollHeight > window.innerHeight * .9){
+            $('#messagediv').scrollTop($('#messagediv')[0].scrollHeight);
+        }
     });
 });
 
